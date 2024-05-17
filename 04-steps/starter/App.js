a2a77220d9bddcf7907b3ca12bc6970751bd26a8
Stanisław Stankiewicz <60826828+stahuOfficial@users.noreply.github.com>
2024-05-18 00:42:21 +0200

    Complete "Steps"

---
+++ 04-steps/starter/App.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    setStep((s) => Math.max(0, s - 1));
+  }
+
+  function handleNext() {
+    setStep((s) => Math.min(2, s + 1));
+  }
+
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 0 ? "active" : ""}>1</div>
+            <div className={step >= 1 ? "active" : ""}>2</div>
+            <div className={step >= 2 ? "active" : ""}>3</div>
+          </div>
+
+          <p className="message">
+            Step {step + 1}: {messages[step]}
+          </p>
+
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>{" "}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
